93626686fd8b411720c208af46f52669d9bf2adc
= <=>
2022-02-03 20:40:30 -0300

    commit

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
@@ -14,7 +15,10 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'image' => $this->faker->imageUrl(),
+            'description' => $this->faker->sentence(5),
+            'user_id' => User::factory()->create()->id
         ];
     }
+    
 }
